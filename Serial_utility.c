bf96e48b6e8320894a8ebf83aa0784a433bf27c4
beeeeep <theprotzect@gmail.com>
2022-04-13 20:06:09 +0200

    Add files via upload

---
+++ Serial_utility.c
@@ -0,0 +1,63 @@
+﻿/*
+ * Serial_utility.c
+ *
+ * Created: 12/2/2021 1:39:19 μμ
+ *  Author: Anestman
+ */ 
+
+#include "Serial_utility.h"
+
+
+
+
+void Serial_init(uint32_t baudrate)
+{
+	usart_rs232_options_t usart_options;
+	usart_options.baudrate=baudrate;
+	usart_options.charlength=USART_CHSIZE_8BIT_gc;
+	usart_options.paritytype=USART_PMODE_DISABLED_gc;
+	usart_options.stopbits=false;				
+	 usart_init_rs232(&USARTD1,&usart_options);
+	 
+	 ioport_set_pin_dir(RX_SERIAL, IOPORT_DIR_INPUT);
+	 ioport_set_pin_dir(TX_SERIAL, IOPORT_DIR_OUTPUT);
+}
+
+void Serial_deactivate(void)
+{
+	sysclk_disable_peripheral_clock(&USARTD1);
+	usart_tx_disable(&USARTD1);
+	usart_rx_disable(&USARTD1);
+	 ioport_set_pin_dir(RX_SERIAL, IOPORT_DIR_INPUT);
+	 ioport_set_pin_dir(TX_SERIAL, IOPORT_DIR_INPUT);
+}
+
+int Serial_put_char(char data, FILE *stream)
+{		
+		usart_putchar(&USARTD1,data);	
+	return 0;  
+}
+
+int Serial_get_char(FILE *stream)
+{
+
+	while (usart_rx_is_complete(&USARTD1) == false)
+	 {
+		 wdt_reset();
+	}
+	
+	return ((uint8_t)(&USARTD1)->DATA);
+
+}
+
+uint8_t Serial_available(void)
+{
+	if((uint8_t)((&USARTD1)->DATA)=='\n')
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
---
+++ Serial_utility.h
@@ -0,0 +1,48 @@
+﻿/*
+ * Serial_utility.h
+ *
+ * Created: 12/2/2021 1:32:03 μμ
+ *  Author: Anestman
+ */ 
+
+
+#ifndef SERIAL_UTILITY_H_
+#define  SERIAL_UTILITY_H_
+
+#include <stdio.h>
+#include <asf.h>
+#include "../ASF/xmega/drivers/usart/usart.h"
+#include "../ASF/xmega/drivers/usart/usart.h"
+#include "../ASF/xmega/drivers/wdt/wdt.h"
+#include "../ASF/xmega/utils/status_codes.h"
+#include "sysclk.h"
+
+#define RX_SERIAL IOPORT_CREATE_PIN(PORTD, 6)
+#define TX_SERIAL IOPORT_CREATE_PIN(PORTD, 7)
+
+
+void Serial_init(uint32_t baudrate);
+void Serial_deactivate(void);
+int Serial_put_char(char data, FILE *stream);
+int Serial_get_char(FILE *stream);
+uint8_t Serial_available(void);
+
+static FILE Serial_print = FDEV_SETUP_STREAM(Serial_put_char, NULL, _FDEV_SETUP_WRITE);
+
+static FILE Serial_read = FDEV_SETUP_STREAM(NULL, Serial_get_char, _FDEV_SETUP_READ);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /*  SERIAL_UTILITY_H_ */
